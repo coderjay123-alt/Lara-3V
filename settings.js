02888be5df6fda59da5c428d1af9335d63be153e
coderjay123-alt <pansilumalshan6@gmail.com>
2025-07-22 17:46:20 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,15 +1,15 @@
-const fs = require('fs');
+qconst fs = require('fs');
 if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
 
 function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "Your_Session_Id", // Add your session id
+SESSION_ID: process.env.SESSION_ID || "3QwgBSIb#reLSfKrkbpQki-yTOUnbx8MZlXCq97NHMBD3zqOzM5c", // Add your session id
 PREFIX: process.env.PREFIX || ".",
 MODE: process.env.MODE || "public",  //public | private | inbox | group
-OWNER_NUMBER: process.env.OWNER_NUMBER || "94779062397",
-DEV: process.env.DEV || "94779062397",
+OWNER_NUMBER: process.env.OWNER_NUMBER || "94779350197",
+DEV: process.env.DEV || "94779350197",
 LANG: process.env.LANG || "EN", // EN | SI
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 AUTO_STATUS_REACT: process.env.AUTO_STATUS_REACT || "true",
